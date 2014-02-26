d604ce761ccf40a169a70175d2adffbb0ed2d2c0
Reed Nelson-Saunders <reedns@hotmail.com>
2014-02-26 10:55:28 -0800

    Delete better_number.rb

--- Chapter 5/better_number.rb
+++
@@ -1,5 +0,0 @@
-puts "Hi, what's your favorite number?"
-number = gets.chomp
-new_number = number.to_i + 1
-better_number = new_number.to_s
-puts 'Try ' + better_number + ' as your new favorite number and let us know how you like it!'
